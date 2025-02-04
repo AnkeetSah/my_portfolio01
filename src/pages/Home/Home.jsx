52c6d60ecb9b503eb09ed2f8fb58d26feda5ac03
AnkeetSah <ankeetkumar7777@gmail.com>
2025-02-04 15:21:46 +0530

    added _redirect file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
-    <div className="bg-primary min-h-[calc(100vh-55px)]">
-  home
-</div>
+    <div className="bg-primary  min-h-[calc(100vh-104px)] flex  justify-center items-center gap-5  ">
+       <div
+        className="h-[400px]  w-[43%] flex flex-col justify-center gap-10 pl-19  "
+       
+      >
+       <div className='w-full '>
+       <p className='text-white'>Hi all. I am</p>
+       <h2 className='text-6xl font-bold text-white'>Ankeet Kumar Sah</h2>
+       <h3 className='text-2xl font-semibold text-[#4D5BCE]'> Front-end developer</h3>
+       </div>
 
-  )
-}
+       <div>
+      <p> // complete the game to continue</p>
+       <p>// you can also see it on my Github page</p>
+       <p>const githubLink = “https://github.com/example/url”</p>
+       </div>
 
-export default Home
+
+      </div>
+      {/* -------------------------------------------------- */}
+       <div
+        className="h-full w-[40%] flex  justify-center items-center"
+      av 
+      >
+        <img className="h-[440px]" src="/assests/images/me.png" alt="Profile" />
+      </div>
+     </div>
+  );
+};
+
+export default Home;
